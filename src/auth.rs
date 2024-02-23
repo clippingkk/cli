0b0e99432807376f9aaad06e746faca6fcf236f1
AnnatarHe <annatar.he+github@gmail.com>
2024-02-24 00:59:07 +0800

    fix(app): upgrade all deps to latest version

--- src/auth.rs
+++ src/auth.rs
@@ -1,41 +1,44 @@
-use inquire::{validator::StringValidator, Text, Password};
+use inquire::{
+    validator::{StringValidator, Validation},
+    Password, Text,
+};
 use std::error::Error;
 
 pub(crate) struct AuthInPrompt {
-	pub email: String,
-	pub password: String
+    pub email: String,
+    pub password: String,
 }
 
 pub(crate) fn get_auth_from_prompt() -> Result<AuthInPrompt, Box<dyn Error>> {
-	let validator: StringValidator = &|input| {
-		let chars_count = input.chars().count();
-		// TODO: check email
-		if chars_count > 64 || chars_count < 6 {
-			Err(String::from("email please~"))
-		} else {
-			Ok(())
-		}
-	};
-	let pwd_validator: StringValidator = &|input| {
-		let chars_count = input.chars().count();
-		// TODO: check email
-		if chars_count > 64 || chars_count < 4 {
-			Err(String::from("password please~"))
-		} else {
-			Ok(())
-		}
-	};
+    let validator = |input: &str| {
+        let chars_count = input.chars().count();
+        // TODO: check email
+        if chars_count > 64 || chars_count < 6 {
+            Ok(Validation::Invalid(("email please~").into()))
+        } else {
+            Ok(Validation::Valid)
+        }
+    };
+    let pwd_validator = |input: &str| {
+        let chars_count = input.chars().count();
+        if chars_count > 64 || chars_count < 4 {
+            Ok(Validation::Invalid(("password please~").into()))
+        } else {
+            Ok(Validation::Valid)
+        }
+    };
 
-	let email = Text::new("What's your email in `clippingkk` ?")
-		.with_validator(validator)
-		.prompt()?;
+    let email = Text::new("What's your email in `clippingkk` ?")
+        .with_validator(validator)
+        .prompt()?;
 
-	let password = Password::new("What's your password that pair with email your previous provided ?")
-		.with_validator(pwd_validator)
-		.prompt()?;
+    let password =
+        Password::new("What's your password that pair with email your previous provided ?")
+            .with_validator(pwd_validator)
+            .prompt()?;
 
-	Ok(AuthInPrompt{
-		email: email,
-		password: password
-	})
+    Ok(AuthInPrompt {
+        email: email,
+        password: password,
+    })
 }
